9fb16d815f3c6a615ceb00f5a04b67e57d6cfe7f
Shivansh <Dwivedi>
2022-02-04 15:14:03 +0530

    Awesome quotes app

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'quotes.dart';
+
+void main() => runApp(MaterialApp(home: QuoteList()));
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+/*'Be yourself , everyone else is already taken .',
+    'I have nothing to declare except my genius.',
+    'The truth is rarely pure and never simple.' */
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        text: 'Be yourself , everyone else is already taken .',
+        author: 'Oscar Wilde'),
+    Quote(
+        text: 'I have nothing to declare except my genius.',
+        author: 'Oscar Wilde'),
+    Quote(
+        text: 'The truth is rarely pure and never simple. ',
+        author: 'Oscar Wilde')
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: const Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => Text('${quote.text} - ${quote.author}'))
+            .toList(),
+      ),
+    );
+  }
+}
---
+++ lib/quotes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// ignore: camel_case_types
+class Quote {
+  String? text;
+  String? author;
+
+  Quote({this.text, this.author});
+}
